9b06c6b4e7d3edc41b8d68b96bba206bff199187
liyifan <lonelytx@gamil.com>
2020-11-22 03:22:55 +0800

    xiugai2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
-
+from config import Config
 app = Flask(__name__)
-
-print('哪个包在使用我：', __name__)
-
+app.config.from_object(Config)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from flask import render_template
 
 from app import app
