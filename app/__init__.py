6d69b937f077e0e5428036adac3216bdc5567250
liyifan <lonelytx@gamil.com>
2020-11-22 19:35:03 +0800

    e
    jiof

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
-print(app.config['SECRET_KEY'])
-from app import routes
+# print(app.config['SECRET_KEY'])
+db= SQLAlchemy(app)
+migrate = Migrate(app,db)
+from app import routes,models
+
