bbbe3d89a2eb64130725d4c374c75d1defb26f11
liyifan <lonelytx@gamil.com>
2020-11-22 19:10:18 +0800

    xiugai2

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,32 @@
-from flask import render_template
+from flask import render_template,redirect,flash
 
 from app import app
+from app.forms import LoginForm
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    loginform = LoginForm()
+    if loginform.validate():
+        print(loginform.username.data)
+        return redirect('/index')
+    return render_template('login.html', title='home', form=loginform)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {
-        'username':'Miguel'
+        'username': 'Miguel'
     }
-    posts = [ # fake array of posts
+    posts = [  # fake array of posts
         {
-            'author': { 'username': 'John' },
+            'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': { 'username': 'Susan' },
+            'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('./index.html',title='home',user=user,posts=posts)
-
-
-
+    return render_template('./index.html', title='home', user=user, posts=posts)
